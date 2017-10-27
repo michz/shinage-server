5c37288b94127f24f7deec02813cd2873e0a4023
Michael Zapf <m.zapf@mztx.de>
2017-10-27 12:00:47 +0200

    move services definitions to AppBundle

---
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * @author   :  Michael Zapf <m.zapf@mztx.de>
+ * @date     :  27.10.17
+ * @time     :  11:59
+ */
+class AppExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
