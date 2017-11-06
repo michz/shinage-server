02ae3b738441d5daddf06c689af72b439c5bb9ff
Michael Zapf <m.zapf@mztx.de>
2017-11-06 20:49:33 +0100

    removed unused ContainerAwareTrait

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -3,18 +3,14 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
  * @author   :  Michael Zapf <m.zapf@mztx.de>
  * @date     :  05.11.17
  * @time     :  17:59
  */
-class Builder implements ContainerAwareInterface
+class Builder
 {
-    use ContainerAwareTrait;
-
     private $factory;
 
     /**
