566578f56a44c6f7b516e2cac1907209d132ba32
Michael Zapf <michi.zapf@gmail.com>
2018-05-19 15:04:05 +0200

    add a orga to fixtures

--- src/AppBundle/DataFixtures/ORM/UserFixtures.php
+++ src/AppBundle/DataFixtures/ORM/UserFixtures.php
@@ -15,16 +15,18 @@ use FOS\UserBundle\Model\UserManagerInterface;
  */
 class UserFixtures extends Fixture
 {
-    const USERNAME_ADMIN = 'admin@shinage.dev';
+    const USERNAME_ADMIN = 'admin@shinage.test';
     const PASSWORD_ADMIN = 'admin';
 
     public function load(ObjectManager $manager)
     {
         $this->createAdmin($manager);
+        $orga1 = $this->createOrga1($manager);
+        $this->createOrgaUser1($manager, $orga1);
         $manager->flush();
     }
 
-    protected function createAdmin(ObjectManager $manager)
+    private function createAdmin(ObjectManager $manager)
     {
         $admin = new User();
         $admin->setUserType(UserType::USER_TYPE_USER);
@@ -33,6 +35,43 @@ class UserFixtures extends Fixture
         $admin->setPlainPassword(self::PASSWORD_ADMIN);
         $admin->setEnabled(true);
         $admin->addRole('ROLE_SUPER_ADMIN');
+
+        /** @var UserManagerInterface $userManager */
+        $userManager = $this->container->get('fos_user.user_manager');
+        $userManager->updateCanonicalFields($admin);
+        $userManager->updatePassword($admin);
+
+        $manager->persist($admin);
+    }
+
+    private function createOrga1($manager)
+    {
+        $orga = new User();
+        $orga->setUserType(UserType::USER_TYPE_ORGA);
+        $orga->setName('Test-Organization 1');
+        $orga->setEmail('orga1@shinage.test');
+        $orga->setPassword('dsfhgkjhdsfkjghdfhgkjdfhgkjdhfjkjghkjdshfgkjhdsfjkghksjdfhgkjdshfkghkjdhkjh');
+        $orga->setEnabled(true);
+
+        /** @var UserManagerInterface $userManager */
+        $userManager = $this->container->get('fos_user.user_manager');
+        $userManager->updateCanonicalFields($orga);
+        $userManager->updatePassword($orga);
+
+        $manager->persist($orga);
+
+        return $orga;
+    }
+
+    private function createOrgaUser1(ObjectManager $manager, User $orga)
+    {
+        $admin = new User();
+        $admin->setUserType(UserType::USER_TYPE_USER);
+        $admin->setName('User 1');
+        $admin->setEmail('user1@shinage.test');
+        $admin->setPlainPassword('user1');
+        $admin->setEnabled(true);
+        $admin->addOrganization($orga);
 
         /** @var UserManagerInterface $userManager */
         $userManager = $this->container->get('fos_user.user_manager');
