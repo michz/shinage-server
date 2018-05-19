624f678bef7aa9eb3df04c9c1e9bfcbd2368d5db
Michael Zapf <m.zapf@mztx.de>
2018-05-19 13:32:57 +0200

    removed multiple docker control scripts and add docker-compose file and one controle script

--- bin/runDevWebserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker network create shinagedevnet
-
-
--- web/app_dev.php
+++ web/app_dev.php
@@ -9,11 +9,18 @@ use shinage\serverApp\AppKernel;
 // for more information
 //umask(0000);
 
+
+var_dump(@$_SERVER['REMOTE_ADDR']);
+
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
+    || (
+        !preg_match('/^172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32)\.|/', @$_SERVER['REMOTE_ADDR'])
+        && !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
+    )
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
