95882cc1a65206b198fa8837f04639f33afc1bdb
Michael Zapf <michi.zapf@gmail.com>
2018-05-20 12:57:32 +0200

    add plain empty new bundle

---
+++ src/ShinageOnlinePlayerBundle/ShinageOnlinePlayerBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace mztx\ShinageOnlinePlayerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShinageOnlinePlayerBundle extends Bundle
+{
+}
