3bf1d4f25481fd181ed3203eea056a4ea5a580cb
Michael Zapf <m.zapf@mztx.de>
2018-09-06 19:38:32 +0200

    move homepage route to yaml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,15 +8,11 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(): Response
     {
         return $this->redirectToRoute('management-dashboard');
