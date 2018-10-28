4024e765ae3e04a9c2d67ab4295cdfa4e520b8a1
Michael Zapf <m.zapf@mztx.de>
2018-10-28 10:29:55 +0100

    implement new APIs files controller

--- src/AppBundle/Controller/Api/v1/FilesController.php
+++ src/AppBundle/Controller/Api/v1/FilesController.php
@@ -12,6 +12,8 @@ use AppBundle\Controller\Api\Exception\AccessDeniedException;
 use AppBundle\Entity\User;
 use AppBundle\Service\FilePoolPermissionCheckerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -23,20 +25,61 @@ class FilesController extends Controller
     /** @var TokenStorageInterface */
     private $tokenStorage;
 
+    /** @var string */
+    private $filePoolBasePath;
+
+    // @TODO Perhaps use flysystem?
+
     public function __construct(
         FilePoolPermissionCheckerInterface $filePoolPermissionChecker,
-        TokenStorageInterface $tokenStorage
+        TokenStorageInterface $tokenStorage,
+        string $filePoolBasePath
     ) {
         $this->filePoolPermissionChecker = $filePoolPermissionChecker;
         $this->tokenStorage = $tokenStorage;
+        $this->filePoolBasePath = $filePoolBasePath;
     }
 
     public function getAction(string $path): Response
     {
         $this->checkPathPermissions($path);
 
-        // @TODO if $path is file, return it (with correct mime type)
-        // @TODO if $path is dir, return listing
+        $fullPath = $this->getFullPath($path);
+
+        // @TODO Handle If-Modified-Since Header if set
+        // @TODO Handle If-Unmodified-Since Header if set
+
+        if (is_dir($fullPath)) {
+            $directoryIterator = new \DirectoryIterator($fullPath);
+            $files = [];
+            foreach ($directoryIterator as $file) {
+                if ($file->isFile()) {
+                    $files[] = $file->getFilename();
+                } elseif ($file->isDir() && false === $file->isDot()) {
+                    $files[] = $file->getFilename() . '/';
+                }
+            }
+            return new Response(
+                json_encode($files),
+                200,
+                [
+                    'Content-Type' => 'text/json',
+                ]
+            );
+        } elseif (is_file($fullPath)) {
+            $file = new File($fullPath);
+            return new Response(
+                file_get_contents($fullPath),
+                200,
+                [
+                    'Content-Type' => $file->getMimeType(),
+                    'Content-Length' => $file->getSize(),
+                    'Last-Modified' => gmdate('D, d M Y G:i:s T', $file->getMTime()),
+                ]
+            );
+        }
+
+        throw new FileNotFoundException($path);
     }
 
     public function putAction(string $path): Response
@@ -63,5 +106,10 @@ class FilesController extends Controller
         if (false === $this->filePoolPermissionChecker->mayUserAccessPath($user, $path)) {
             throw new AccessDeniedException();
         }
+    }
+
+    private function getFullPath(string $filePath): string
+    {
+        return $this->filePoolBasePath . $filePath;
     }
 }
