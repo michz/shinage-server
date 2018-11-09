79a7be5b142c98dbc3d3800dd5bae32e63e806c6
Michael Zapf <m.zapf@mztx.de>
2018-11-09 22:36:31 +0100

    minor cleanup in controller

--- src/Controller/Presentation/RenderPresentationController.php
+++ src/Controller/Presentation/RenderPresentationController.php
@@ -10,29 +10,22 @@ namespace App\Controller\Presentation;
 
 use App\Entity\Presentation;
 use App\Presentation\AnyPresentationRendererInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class RenderPresentationController extends Controller
 {
-    /** @var EntityManagerInterface */
-    private $entityManager;
-
     /** @var AnyPresentationRendererInterface */
     private $anyPresentationRenderer;
 
     public function __construct(
-        EntityManagerInterface $entityManager,
         AnyPresentationRendererInterface $anyPresentationRenderer
     ) {
-        $this->entityManager = $entityManager;
         $this->anyPresentationRenderer = $anyPresentationRenderer;
     }
 
-    public function getAction(int $id): Response
+    public function getAction(Presentation $presentation): Response
     {
-        $presentation = $this->entityManager->find(Presentation::class, $id);
         return new Response(
             $this->anyPresentationRenderer->render($presentation),
             200
