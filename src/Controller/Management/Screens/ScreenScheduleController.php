eabd65eedee62619f5a9530049d9449ea1a58fe8
Michael Zapf <m.zapf@mztx.de>
2018-11-11 21:01:45 +0100

    major scheduler refactoring
    * make jquery plugin to be able to use scheduler at different places
    * use scheduler in screen settings

--- src/Controller/Management/Screens/ScreenScheduleController.php
+++ src/Controller/Management/Screens/ScreenScheduleController.php
@@ -9,21 +9,35 @@ declare(strict_types=1);
 namespace App\Controller\Management\Screens;
 
 use App\Entity\Screen;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ScreenScheduleController extends Controller
 {
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TokenStorageInterface $tokenStorage
+    ) {
+        $this->entityManager = $entityManager;
+        $this->tokenStorage = $tokenStorage;
+    }
+
     public function indexAction(string $guid): Response
     {
-        /** @var EntityManager $em */
-        $em = $this->getDoctrine()->getManager();
-
-        $screen = $em->find(Screen::class, $guid);
+        $user = $this->tokenStorage->getToken()->getUser();
+        $screen = $this->entityManager->find(Screen::class, $guid);
 
         return $this->render('manage/screens/schedule.html.twig', [
             'screen' => $screen,
+            'presentations' => $user->getPresentations($this->entityManager),
         ]);
     }
 }
