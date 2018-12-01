612fa0d82f5f08b4416b0dbf1feb764d54cf4704
Michael Zapf <m.zapf@mztx.de>
2018-12-01 12:00:45 +0100

    change screen settings to use symfony forms

--- src/Controller/Management/Screens/ScreenDataController.php
+++ src/Controller/Management/Screens/ScreenDataController.php
@@ -11,6 +11,9 @@ namespace App\Controller\Management\Screens;
 use App\Entity\Screen;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -25,7 +28,7 @@ class ScreenDataController extends Controller
         $this->entityManager = $entityManager;
     }
 
-    public function indexAction(string $guid): Response
+    public function indexAction(Request $request, string $guid): Response
     {
         // TODO Check if logged in user is allowed to edit screen.
 
@@ -37,36 +40,34 @@ class ScreenDataController extends Controller
         if ($this->isGranted('manage', $screen)) {
             $readonlyAttr = '';
             $readonly = false;
+
+            $form = $this->createFormBuilder($screen, ['translation_domain' => 'ScreenSettings'])
+                ->add('name', TextType::class)
+                ->add('location', TextType::class)
+                ->add('adminc', TextType::class)
+                ->add('notes', TextareaType::class)
+                ->add('save', SubmitType::class)
+                ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->addFlash('success', 'Saved successfully');
+                $this->entityManager->flush();
+            }
+        } else {
+            $form = $this->createFormBuilder($screen, ['translation_domain' => 'ScreenSettings'])
+                ->add('name', TextType::class, ['attr' => ['disabled' => 'disabled']])
+                ->add('location', TextType::class, ['attr' => ['disabled' => 'disabled']])
+                ->add('adminc', TextType::class, ['attr' => ['disabled' => 'disabled']])
+                ->add('notes', TextareaType::class, ['attr' => ['disabled' => 'disabled']])
+                ->getForm();
         }
 
         return $this->render('manage/screens/data.html.twig', [
+            'form' => $form->createView(),
             'screen' => $screen,
             'readonlyAttr' => $readonlyAttr,
             'readonly' => $readonly,
         ]);
-    }
-
-    public function saveDataAction(Request $request, string $guid): Response
-    {
-        $name = $request->get('txtName');
-        $loc = $request->get('txtLocation');
-        $notes = $request->get('txtNotes');
-        $admin = $request->get('txtAdmin');
-
-        $screen = $this->entityManager->find(Screen::class, $guid);
-
-        // Check if screen may be edited by current user
-        $this->denyAccessUnlessGranted('manage', $screen);
-
-        $screen->setName($name);
-        $screen->setNotes($notes);
-        $screen->setLocation($loc);
-        $screen->setAdminC($admin);
-
-        $this->entityManager->persist($screen);
-        $this->entityManager->flush();
-
-        $this->addFlash('success', 'Screen data saved.');
-        return $this->redirect($request->get('hidUri'));
     }
 }
