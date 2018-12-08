fc885df201f53a3b1aa080b4cb9cc439266f7ec0
Michael Zapf <m.zapf@mztx.de>
2018-12-08 15:49:25 +0100

    add list presentations api route + implement put

--- src/Controller/Api/v1/PresentationsController.php
+++ src/Controller/Api/v1/PresentationsController.php
@@ -9,15 +9,20 @@ declare(strict_types=1);
 namespace App\Controller\Api\v1;
 
 use App\Entity\Presentation;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class PresentationsController extends AbstractController
 {
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
     /** @var EntityManagerInterface */
     private $entityManager;
 
@@ -25,31 +30,72 @@ class PresentationsController extends AbstractController
     private $serializer;
 
     public function __construct(
+        TokenStorageInterface $tokenStorage,
         EntityManagerInterface $entityManager,
         SerializerInterface $serializer
     ) {
+        $this->tokenStorage = $tokenStorage;
         $this->entityManager = $entityManager;
         $this->serializer = $serializer;
     }
 
-    public function getAction(Request $request, int $id): Response
+    public function listAction(): Response
+    {
+        /** @var User $user */
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+
+        $allowedIds = [$user->getId()];
+        $allowed = [$user];
+        foreach ($user->getOrganizations() as $organization) {
+            $allowedIds[] = $organization->getId();
+            $allowed[] = $organization;
+        }
+
+        $queryBuilder
+            ->select('p')
+            ->from(Presentation::class, 'p')
+            ->where($queryBuilder->expr()->in('p.owner', $allowedIds));
+
+        $presentations = $queryBuilder->getQuery()->execute();
+
+        return new Response(
+            $this->serializer->serialize($presentations, 'json'),
+            200,
+            [
+                'Content-Type' => 'application/json; charset=UTF-8',
+            ]
+        );
+    }
+
+    public function getAction(int $id): Response
     {
         $presentation = $this->entityManager->find(Presentation::class, $id);
         if (null === $presentation) {
             throw new NotFoundHttpException('Presentation with id `' . $id . '` not found.');
         }
 
-        // @TODO Implement voter
         $this->denyAccessUnlessGranted('get', $presentation);
 
         return new Response(
             $this->serializer->serialize($presentation, 'json'),
-            200
+            200,
+            [
+                'Content-Type' => 'application/json; charset=UTF-8',
+            ]
         );
     }
 
-    public function putAction(Request $request, int $id): Response
+    public function putAction(Request $request): Response
     {
+        /** @var Presentation $presentation */
+        $presentation = $this->serializer->deserialize($request->getContent(), Presentation::class, 'json');
+        $this->denyAccessUnlessGranted('put', $presentation);
+
+        $presentation->setLastModified(new \DateTime());
+        $this->entityManager->flush();
+        return new Response('', 204);
     }
 
     public function deleteAction(int $id): Response
@@ -59,7 +105,6 @@ class PresentationsController extends AbstractController
             throw new NotFoundHttpException('Presentation with id `' . $id . '` not found.');
         }
 
-        // @TODO Implement voter
         $this->denyAccessUnlessGranted('delete', $presentation);
 
         $this->entityManager->remove($presentation);
