c40185e6a97ca53b75272006aa00cb8a0f2f89d5
Michael Zapf <m.zapf@mztx.de>
2019-01-01 12:23:19 +0100

    scheduler collision: add check for enclosing same
    + refactor full enclosing check

--- src/Service/ScheduleCollisionHandler.php
+++ src/Service/ScheduleCollisionHandler.php
@@ -20,6 +20,12 @@ class ScheduleCollisionHandler implements ScheduleCollisionHandlerInterface
     /** @var Query */
     private $selectEnclosedQuery;
 
+    /** @var Query */
+    private $selectOtherEnclosingQuery;
+
+    /** @var Query */
+    private $selectSameEnclosingQuery;
+
     public function __construct(
         EntityManagerInterface $entityManager
     ) {
@@ -30,8 +36,20 @@ class ScheduleCollisionHandler implements ScheduleCollisionHandlerInterface
 
     private function prepareQueries(): void
     {
-        $selectEnclosedQueryBuilder = $this->entityManager->createQueryBuilder();
+        $selectSameEnclosingQueryBuilder = $this->entityManager->createQueryBuilder();
+        $this->selectSameEnclosingQuery =
+            $selectSameEnclosingQueryBuilder
+                ->select('scheduledPresentation')
+                ->from(ScheduledPresentation::class, 'scheduledPresentation')
+                ->where($selectSameEnclosingQueryBuilder->expr()->lt('scheduledPresentation.scheduled_start', ':current_start'))
+                ->andWhere($selectSameEnclosingQueryBuilder->expr()->gt('scheduledPresentation.scheduled_end', ':current_end'))
+                ->andWhere($selectSameEnclosingQueryBuilder->expr()->eq('scheduledPresentation.screen', ':screen'))
+                ->andWhere($selectSameEnclosingQueryBuilder->expr()->neq('scheduledPresentation.id', ':id'))
+                ->andWhere($selectSameEnclosingQueryBuilder->expr()->eq('scheduledPresentation.presentation', ':presentation'))
+                ->orderBy('scheduledPresentation.scheduled_start', 'ASC')
+                ->getQuery();
 
+        $selectEnclosedQueryBuilder = $this->entityManager->createQueryBuilder();
         $this->selectEnclosedQuery =
             $selectEnclosedQueryBuilder
                 ->select('scheduledPresentation')
@@ -42,12 +60,39 @@ class ScheduleCollisionHandler implements ScheduleCollisionHandlerInterface
                 ->andWhere($selectEnclosedQueryBuilder->expr()->neq('scheduledPresentation.id', ':id'))
                 ->orderBy('scheduledPresentation.scheduled_start', 'ASC')
                 ->getQuery();
+
+        $selectOtherEnclosingQueryBuilder = $this->entityManager->createQueryBuilder();
+        $this->selectOtherEnclosingQuery =
+            $selectOtherEnclosingQueryBuilder
+                ->select('scheduledPresentation')
+                ->from(ScheduledPresentation::class, 'scheduledPresentation')
+                ->where($selectOtherEnclosingQueryBuilder->expr()->lt('scheduledPresentation.scheduled_start', ':current_start'))
+                ->andWhere($selectOtherEnclosingQueryBuilder->expr()->gt('scheduledPresentation.scheduled_end', ':current_end'))
+                ->andWhere($selectOtherEnclosingQueryBuilder->expr()->eq('scheduledPresentation.screen', ':screen'))
+                ->andWhere($selectOtherEnclosingQueryBuilder->expr()->neq('scheduledPresentation.id', ':id'))
+                ->orderBy('scheduledPresentation.scheduled_start', 'ASC')
+                ->getQuery();
     }
 
     public function handleCollisions(ScheduledPresentation $s): void
     {
         $start = $s->getScheduledStart();
         $end = $s->getScheduledEnd();
+
+        // First check if new presentation is entirely enclosed by the same presentation => remove the new one
+        $overlaps = $this->selectSameEnclosingQuery
+            ->execute([
+                'current_start' => $start,
+                'current_end'   => $end,
+                'id'            => $s->getId(),
+                'screen'        => $s->getScreen(),
+                'presentation'  => $s->getPresentation(),
+            ]);
+
+        if (false === empty($overlaps)) {
+            $this->entityManager->remove($s);
+            return;
+        }
 
         // check if scheduled presentation encloses same/other schedule on same screen entirely
         $overlaps = $this->selectEnclosedQuery
@@ -67,25 +112,16 @@ class ScheduleCollisionHandler implements ScheduleCollisionHandlerInterface
         $this->entityManager->flush();
 
         // check if scheduled presentation is entirely enclosed by same/other schedule on same screen
-        $query = $this->entityManager->createQuery(
-            'SELECT p
-                    FROM App:ScheduledPresentation p
-                    WHERE
-                        (
-                        (p.scheduled_start < :current_start AND p.scheduled_end > :current_end)
-                        ) AND 
-                        p.screen = :screen
-                        AND 
-                        p.id != :id
-                    ORDER BY p.scheduled_start ASC'
-        )
-            ->setParameter('current_start', $start)
-            ->setParameter('current_end', $end)
-            ->setParameter('id', $s->getId())
-            ->setParameter('screen', $s->getScreen());
+        $overlaps = $this->selectOtherEnclosingQuery
+            ->execute([
+                'current_start' => $start,
+                'current_end'   => $end,
+                'id'            => $s->getId(),
+                'screen'        => $s->getScreen(),
+            ]);
 
-        $overlaps = $query->getResult();
-        foreach ($overlaps as $o) { /** @var ScheduledPresentation $o */
+        /** @var ScheduledPresentation $o */
+        foreach ($overlaps as $o) {
             $new_o = new ScheduledPresentation();
             $new_o->setScreen($o->getScreen());
             $new_o->setPresentation($o->getPresentation());
@@ -101,6 +137,8 @@ class ScheduleCollisionHandler implements ScheduleCollisionHandlerInterface
         }
 
         $this->entityManager->flush();
+
+        return;
 
         // check if scheduled presentation overlaps same/other schedule on same screen
         $query = $this->entityManager->createQuery(
