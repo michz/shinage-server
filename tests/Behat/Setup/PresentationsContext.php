49064fae26e8fce625f4cd688a0b8dcb5e126df6
Michael Zapf <m.zapf@mztx.de>
2019-09-29 19:50:30 +0200

    behat: move setup contexts to new folder structure

--- tests/Behat/Setup/PresentationsContext.php
+++ tests/Behat/Setup/PresentationsContext.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * Licensed under MIT. See file /LICENSE.
  */
 
-namespace shinage\server\behat\Setup;
+namespace Tests\Behat\Setup;
 
 use App\Entity\Presentation;
 use App\Entity\ScheduledPresentation;
