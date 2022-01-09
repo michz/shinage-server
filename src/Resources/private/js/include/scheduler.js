f41aca107026d30dccd70b63efa25cf818dc1021
Michael Zapf <m.zapf@mztx.de>
2022-01-09 16:08:00 +0100

    upgrade js dependencies

--- src/Resources/private/js/include/scheduler.js
+++ src/Resources/private/js/include/scheduler.js
@@ -54,85 +54,104 @@
     // Avoid Plugin.prototype conflicts
     $.extend(Plugin.prototype, {
         init: function () {
-            var that = this;
-            var today = moment();
-            var tmpView = 'agendaWeek';
-            var vars = {};
+            const that = this;
+            let today = new Date();
+            let tmpView = 'timeGridWeek';
+            let vars = {};
             window.location.hash.split("&").forEach(function(v) {
-                var stripped = v.replace(new RegExp('^[#]+'), '');
-                var splitted = stripped.split('=');
+                const splitted = v.replace(new RegExp('^[#]+'), '').split('=');
                 vars[splitted[0]] = splitted[1];
             });
 
             // @TODO Represent visible screens in URL
-
             Object.keys(vars).forEach(function (key) {
                 var value = vars[key];
                 switch (key) {
                     case 'year':
-                        today.year(parseInt(value, 10));
+                        today.setFullYear(parseInt(value, 10));
                         break;
                     case 'month':
-                        today.month(parseInt(value, 10) - 1);
+                        today.setMonth(parseInt(value, 10) - 1);
                         break;
                     case 'day':
-                        today.date(parseInt(value, 10));
+                        today.setDate(parseInt(value, 10));
                         break;
                     case 'view':
                         tmpView = value;
                         break;
                     case 'selectedScreen':
+                        // @TODO Adjust for new fullcalendar version
                         that.setSelectedScreen(value);
                         break;
                 }
             });
 
-            $('.calendar', this.element).fullCalendar({
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
-                },
-                height: 'auto',
-                locale: 'de',
-                defaultView: tmpView,
-                defaultDate: today,
-                viewRender: $.proxy(function (view) {
-                    this.setUrl(view);
-                }, this),
-                navLinks: true,     // can click day/week names to navigate views
-                editable: true,
-                eventLimit: true,   // allow "more" link when too many events
-                nowIndicator: true,
+            this.calendarElement = $('.calendar', this.element);
+
+            // eslint-disable-next-line no-undef
+            this.calendar = new FullCalendar.Calendar(this.calendarElement.get(0), {
                 allDaySlot: false,
-                snapDuration: '00:05:00',
-                events: [],
-                selectable: true,
-                selectHelper: true,
-                select: $.proxy(this.placePresentationBySelection, this),
+                datesSet: () => {
+                    this.setUrl();
+                },
+                editable: true,
                 eventResize: $.proxy(this.resizePresentation, this),
-                eventRender: $.proxy(function(event, element) {
-                    if (event.className[0] == 'fc-helper') {
-                        var col = this.screen_colors[$(this.getSelectedScreen()).data('color-set')];
-                        $(element).css('background-color', col.dark);
-                        $(element).css('color', col.light);
-                    }
-                    if (!event.presentation) {
-                        return;
+                eventDrop: $.proxy(this.movePresentation, this),
+                eventClick: $.proxy(this.clickScheduledPresentation, this),
+                eventContent: $.proxy(function(contentEvent) {
+                    let title = '';
+                    const event = contentEvent.event;
+
+                    if ('presentation' in contentEvent.event.extendedProps) {
+                        title = `
+                            <div class="fc-event-title-container">
+                                <div class="fc-event-title fc-sticky">${contentEvent.event.extendedProps.presentation.title}</div>
+                            </div>
+                        `;
                     }
 
-                    element.append('<div class="fc-event-title">' + event.presentation.title + '</div>');
+                    return { html: `
+                        <div class="fc-event-main-frame">
+                            <div class="fc-event-time">
+                                ${String(event.start.getHours()).padStart(2, '0')}:${String(event.start.getMinutes()).padStart(2, '0')} -
+                                ${String(event.end.getHours()).padStart(2, '0')}:${String(event.end.getMinutes()).padStart(2, '0')}
+                            </div>
+                            ${title}
+                        </div>
+                    ` };
                 }, this),
-                eventDrop: $.proxy(this.movePresentation, this),
-                eventClick: $.proxy(this.clickScheduledPresentation, this)
+                eventDidMount: $.proxy(function(contentEvent) {
+                    if (contentEvent.isMirror) {
+                        var col = this.screen_colors[$(this.getSelectedScreen()).data('color-set')];
+                        $(contentEvent.el)
+                            .css('background-color', col.dark)
+                            .css('color', col.light);
+                    }
+                }, this),
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                },
+                height: 'auto',
+                initialDate: today,
+                initialView: tmpView,
+                locale: 'de',
+                nowIndicator: true,
+                select: $.proxy(this.placePresentationBySelection, this),
+                selectable: true,
+                selectMirror: true,
+                slotDuration: '00:30:00',
+                snapDuration: '00:05:00'
             });
+            this.calendar.render();
 
             $('.sched-screen-list .item', this.element).on("click", $.proxy(function (e) {
                 this.setSelectedScreen($(e.currentTarget).data('guid'));
-                this.setUrl($('.calendar', this.element).fullCalendar('getView'));
+                this.setUrl();
             }, this));
             $('.sched-screen-list .item .selector', this.element).on("click", $.proxy(function (e) {
-                var button = $(e.currentTarget).parent();
+                const button = $(e.currentTarget).parent();
                 button.toggleClass('visible');
                 e.stopPropagation();
 
@@ -140,21 +159,23 @@
                     this.addEventSource(button.get(0), button.data('event-src'));
                     this.setScreenColor(button.get(0));
                 } else {
-                    $('.calendar', this.element).fullCalendar('removeEventSource', button.data('event-src'));
+                    const eventSource = this.calendar.getEventSourceById(button.data('event-src'));
+                    if (undefined !== eventSource) {
+                        eventSource.remove();
+                    }
+
                     this.setScreenColor(button.get(0));
                 }
             }, this));
-
 
             // If no screen is selected yet, preselect the first
             if ($('.sched-screen-list .selected', this.element).length < 1) {
                 $('.sched-screen-list .item:first', this.element).trigger('click');
             }
 
-            var global_i = 0;
+            let global_i = 0;
             $('.sched-screen-list .item', this.element).each($.proxy(function (k, o) {
-                var i = global_i % this.screen_colors_count;
-                $(o).data('color-set', i);
+                $(o).data('color-set', global_i % this.screen_colors_count);
                 this.setScreenColor(o);
                 global_i++;
             }, this));
@@ -166,7 +187,7 @@
             }, this));
 
             $('#scheduler-diag-edit .action--delete').on('click', function (e) {
-                var scheduledPresentation = $(e.currentTarget).closest('.modal')
+                const scheduledPresentation = $(e.currentTarget).closest('.modal')
                     .find('input[name="scheduledPresentationId"]').data('scheduled-presentation');
                 that.hideEditDialog();
                 that.deletePresentation(scheduledPresentation);
@@ -174,58 +195,41 @@
 
             this.initEditDialog();
         },
-        setUrl: function (view) {
-            var moment = view.start;
-            if (moment) {
-                window.location.hash =
-                    'year=' + moment.format('YYYY') +
-                    '&month=' + moment.format('M') +
-                    '&day=' + moment.format('DD') +
-                    '&view=' + view.name +
-                    '&selectedScreen=' + $(this.getSelectedScreen()).data('guid');
-            }
+        setUrl: function () {
+            window.location.hash =
+                'year=' + this.calendar.view.currentStart.getFullYear() +
+                '&month=' + (this.calendar.view.currentStart.getMonth() + 1) +
+                '&day=' + this.calendar.view.currentStart.getDate() +
+                '&view=' + this.calendar.view.type +
+                '&selectedScreen=' + $(this.getSelectedScreen()).data('guid');
         },
         addEventSource: function (screen, id) {
-            var colset = this.screen_colors[$(screen).data('color-set')];
-            var bg_col = colset.dark;
-            var fg_col = colset.light;
-            $('.calendar', this.element).fullCalendar('addEventSource',
-                {
-                    id: id,
-                    url: this.url_get_schedule,
-                    type: 'post',
-                    data: {
-                        screen:  $(screen).data('guid')
-                    },
-                    error: function () {
-                        // @TODO use notify
-                        alert('there was an error while fetching events!');
-                    },
-                    color: bg_col,
-                    textColor: fg_col
-                }
-            );
+            const colset = this.screen_colors[$(screen).data('color-set')];
+            this.calendar.addEventSource({
+                id: id,
+                url: this.url_get_schedule,
+                method: 'POST',
+                extraParams: {
+                    screen:  $(screen).data('guid')
+                },
+                error: function () {
+                    $.notify("Leider ist beim Laden der Einträge ein Fehler aufgetreten.", "error");
+                },
+                color: colset.dark,
+                textColor: colset.light
+            });
         },
-        saveChanged: function (event, revertFunc) {
+        saveChanged: function (resizeEvent) {
             // @TODO Read messages from translations
             window.ajaxLoadShow();
             $.ajax({
                 url: this.url_change_scheduled,
                 method: 'post',
-                data: {
-                    'id':           event.id,
-                    'screen':       event.screen,
-                    'presentation': event.title,
-                    'start':        event.start.format('YYYY-MM-DD HH:mm:ss'),
-                    'end':          event.end.format('YYYY-MM-DD HH:mm:ss')
-                }
+                data: this.getInternalScheduleEntryFromFullCalendarEvent(resizeEvent.event)
             }).fail(function() {
-                if (revertFunc != undefined) {
-                    revertFunc();
-                }
                 $.notify("Leider ist beim Ändern des Eintrags ein Fehler aufgetreten.", "error");
             }).done(function() {
-                $('.calendar', this.element).fullCalendar('refetchEventSources', event.source);
+                resizeEvent.event.source.refetch();
                 $.notify("Änderung gespeichert.", "success");
             }).always(function () {
                 window.ajaxLoadHide();
@@ -242,7 +246,7 @@
             $('.sched-screen-list .item[data-guid="' + guid + '"]', this.element).addClass('selected');
         },
         setScreenColor: function (screen) {
-            var col = this.screen_colors[$(screen).data('color-set')];
+            const col = this.screen_colors[$(screen).data('color-set')];
             if ($(screen).hasClass('visible')) {
                 $(screen).css('background-color', col.dark);
                 $(screen).css('color', col.light);
@@ -252,19 +256,21 @@
                 $(screen).css('color', col.normal);
             }
         },
-        placePresentationBySelection: function (start, end) {
-            this.showEditDialog(undefined, start, end);
+        placePresentationBySelection: function (info) {
+            this.showEditDialog(undefined, info.start, info.end);
         },
-        resizePresentation: function(event, delta, revertFunc) {
-            this.saveChanged(event, revertFunc);
+        resizePresentation: function(resizeEvent) {
+            this.saveChanged(resizeEvent);
         },
         movePresentation: function(event, delta, revertFunc) {
             this.saveChanged(event, revertFunc);
         },
         deletePresentation: function (event) {
+            const that = this;
+
             // @TODO Move this to dialog or anything other translatable
-            var r = confirm("Soll die Präsentation  \"" + event.presentation.title + "\" (" +
-                event.start.format('DD.MM.YYYY HH:mm') + ") wirklich entfernt werden?");
+            const r = confirm("Soll die Präsentation  \"" + event.presentation.title + "\" (" +
+                event.start + ") wirklich entfernt werden?");
 
             if (r !== true) {
                 return;
@@ -278,7 +284,7 @@
                     'id':  event.id
                 }
             }).done(function() {
-                $('.calendar', this.element).fullCalendar('refetchEventSources', event.source);
+                that.calendar.refetchEvents();
                 $.notify("Präsentation entfernt.", "success");
             }).always(function () {
                 window.ajaxLoadHide();
@@ -286,30 +292,29 @@
         },
 
         clickScheduledPresentation: function (e) {
-            //this.initEditDialog();
-            this.showEditDialog(e);
+            this.showEditDialog(e.event);
         },
 
         hideEditDialog: function () {
             $('#scheduler-diag-edit').modal('hide');
         },
 
-        showEditDialog: function (scheduledPresentation, start, stop) {
-            var $schedulerDialog = $('#scheduler-diag-edit');
+        showEditDialog: function (event, start, stop) {
+            const $schedulerDialog = $('#scheduler-diag-edit');
 
-            if (scheduledPresentation !== undefined) {
+            if (event !== undefined) {
                 // Edit an existing schedule item
                 $schedulerDialog.find('.action--delete').css('visibility', 'visible');
                 $schedulerDialog.data('messageSuccess', $schedulerDialog.data('messageEditSuccess'));
                 $schedulerDialog.data('messageError', $schedulerDialog.data('messageEditError'));
 
-                $schedulerDialog.find('input[name="scheduledPresentationId"]').val(scheduledPresentation.id);
-                $schedulerDialog.find('input[name="scheduledPresentationId"]').data('scheduled-presentation', scheduledPresentation);
-                $schedulerDialog.find('select[name="presentation"]').val(scheduledPresentation.presentation.id);
-                $schedulerDialog.find('select[name="screen"]').val(scheduledPresentation.screen);
+                $schedulerDialog.find('input[name="scheduledPresentationId"]').val(event.id);
+                $schedulerDialog.find('input[name="scheduledPresentationId"]').data('scheduled-presentation', this.getInternalScheduleEntryFromFullCalendarEvent(event, false));
+                $schedulerDialog.find('select[name="presentation"]').val(event.extendedProps.presentation.id);
+                $schedulerDialog.find('select[name="screen"]').val(event.extendedProps.screen);
 
-                start = scheduledPresentation.start;
-                stop = scheduledPresentation.end;
+                start = event.start;
+                stop = event.end;
             } else {
                 // Create a new schedule item
                 $schedulerDialog.find('.action--delete').css('visibility', 'hidden');
@@ -324,10 +329,31 @@
                 $schedulerDialog.find('select[name="screen"]').val(this.getSelectedScreenGuid());
             }
 
-            $schedulerDialog.find('input[name="start"]').val(start.format('DD.MM.YYYY HH:mm'));
-            $schedulerDialog.find('input[name="end"]').val(stop.format('DD.MM.YYYY HH:mm'));
+            $schedulerDialog.find('input[name="start"]').val(this.formatDateTime(start));
+            $schedulerDialog.find('input[name="end"]').val(this.formatDateTime(stop));
 
             $schedulerDialog.modal('show');
+        },
+
+        getInternalScheduleEntryFromFullCalendarEvent: function (event, flat = true) {
+            let r = event.toPlainObject({ collapseExtendedProps: true });
+            if (flat) {
+                r.presentation = r.presentation.id;
+            }
+
+            r.start = this.formatDateTime(event.start);
+            r.end = this.formatDateTime(event.end);
+
+            return r;
+        },
+
+        formatDateTime: function (date) {
+            return date.getFullYear()  + "-" +
+                String(date.getMonth() + 1).padStart(2, '0') + "-" +
+                String(date.getDate()).padStart(2, '0') + " " +
+                String(date.getHours()).padStart(2, '0') + ":" +
+                String(date.getMinutes()).padStart(2, '0') + ":" +
+                String(date.getSeconds()).padStart(2, '0');
         },
 
         initEditDialog: function () {
@@ -350,12 +376,12 @@
                         },
                         complete: function () {
                             window.ajaxLoadHide();
-                            $('.calendar', that.element).fullCalendar('refetchEvents');
+                            that.calendar.refetchEvents();
                         }
                     });
                 },
                 onHide: function () {
-                    $('.calendar', that.element).fullCalendar('unselect');
+                    that.calendar.unselect();
                 },
                 onHidden: function () {
                     $('form', this).trigger('reset');
