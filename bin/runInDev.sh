d332d1b960768a8ed632123b110066a5b2c7901c
Michael Zapf <m.zapf@mztx.de>
2023-04-15 12:10:16 +0200

    update dev env to php 8.2 and adjust for newer docker version

--- bin/runInDev.sh
+++ bin/runInDev.sh
@@ -3,6 +3,6 @@
 
 PROJECT_DIR="$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}") )" && pwd )"
 PROJECT_NAME="$( basename ${PROJECT_DIR} )"
-PHP_CONTAINER_NAME="${PROJECT_NAME}_php_1"
+PHP_CONTAINER_NAME="${PROJECT_NAME}-php-1"
 
 docker exec --workdir /usr/share/nginx/html -it ${PHP_CONTAINER_NAME} $@
